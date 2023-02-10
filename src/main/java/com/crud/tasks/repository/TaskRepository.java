99f5e19bacf928a2ca9ab36c0241f267e412749a
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2023-02-10 18:49:07 +0100

    19.2 improving

--- src/main/java/com/crud/tasks/repository/TaskRepository.java
+++ src/main/java/com/crud/tasks/repository/TaskRepository.java
@@ -5,10 +5,12 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface TaskRepository extends CrudRepository<Task,Long> {
 List<Task> findAll();
-List<Task>findTaskBy(Long id);
+Optional<Task> findById(Long id);
+
 
 }
