d77db01637e0fa0daa451f2cbbfe92b9875bf5e2
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2023-04-24 10:56:17 +0200

    32.2 improving

--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -3,9 +3,11 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class TrelloCardDto {
     private String name;
     private String description;
