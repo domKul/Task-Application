58f41cc96284ce159e80ce2b8eb631695f9a1bf0
Dominik Kulpowicz <dominik.kulpowicz@gmail.com>
2023-04-25 20:30:53 +0200

    32.2 improving

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,12 +1,15 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.scheduler.EmailScheduler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
 
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
@@ -22,13 +25,12 @@ public class MailCreatorService {
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
-        int hour = 6;
-        String result ;
 
         List<String> functionality = new ArrayList<>();
         functionality.add("You can manage your tasks");
         functionality.add("Provides connection with Trello Account");
         functionality.add("Application allows sending tasks to Trello");
+
 
         Context context = new Context();
         context.setVariable("message", message);
@@ -42,14 +44,9 @@ public class MailCreatorService {
         context.setVariable("admin_config", adminConfig);
         context.setVariable("application_functionality", functionality);
 
-        Calendar calendar = Calendar.getInstance();
 
-        if (calendar.get(Calendar.HOUR_OF_DAY)>= hour){
-            result = templateEngine.process("mail/daily_information",context);
-        }else{
-            result = templateEngine.process("mail/created-trello-card-mail", context);
-        }
-        return result;
+        return templateEngine.process("mail/created-trello-card-mail", context);
+
 
     }
 
